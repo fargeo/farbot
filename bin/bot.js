38ff19e1ab2265eebf5d256d57a4b9177cdcf43c
Jeffrey Munowitch <jmunowitch@fargeo.com>
2016-02-18 12:18:14 -0800

    More Heroku Testing

--- bin/bot.js
+++ bin/bot.js
@@ -10,6 +10,7 @@
 
 var FarBot = require('../lib/farbot');
 var later = require('later');
+var express = require('express');
 
 /**
 * Environment variables used to configure the bot:
@@ -32,11 +33,16 @@ var farbot = new FarBot({
 farbot.run();
 
 //required for Heroku
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.send('it is running\n');
-}).listen(process.env.PORT || 5000);
+
+var app     = express();
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 
 later.date.localTime();
 
