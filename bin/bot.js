c62b19c328317fd85d1526f85414b0886ef1219a
Jeffrey Munowitch <jmunowitch@fargeo.com>
2016-02-18 11:56:19 -0800

    Update to bind the bot to an HTTP port for Heroku

--- bin/bot.js
+++ bin/bot.js
@@ -31,6 +31,13 @@ var farbot = new FarBot({
 
 farbot.run();
 
+//required for Heroku
+var http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.send('it is running\n');
+}).listen(process.env.PORT || 5000);
+
 later.date.localTime();
 
 var sched = later.parse.text('at 10:30 am on Tues and Thurs'),
