31fefb90d4098aa550e5389fc160027ba8fc8916
Jeffrey Munowitch <jmunowitch@fargeo.com>
2016-02-18 12:04:44 -0800

    More changes with Heroku

--- bin/bot.js
+++ bin/bot.js
@@ -34,8 +34,8 @@ farbot.run();
 //required for Heroku
 var http = require('http');
 http.createServer(function (req, res) {
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.send('it is running\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.send('it is running\n');
 }).listen(process.env.PORT || 5000);
 
 later.date.localTime();
